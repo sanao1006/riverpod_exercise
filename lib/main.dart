3a1e5b5fb1f05e8b9785440658334e417ee29243
sanao1006 <naosak1006@gmail.com>
2025-05-06 22:24:09 +0900

    feat: add asynchronous greeting and fake API calls with Riverpod integration

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,70 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'main.g.dart';
 
 @riverpod
+Future<String> asyncGreeting(AsyncGreetingRef ref) async {
+  await Future<void>.delayed(const Duration(seconds: 1));
+  return 'Hello, world!';
+}
+
+@Riverpod(keepAlive: true)
 class CounterNotifier extends _$CounterNotifier {
   @override
-  int build() => 0;
+  Future<int> build() async {
+    await Future<void>.delayed(const Duration(seconds: 1));
+    return 0;
+  }
 
-  void increment() => state++;
+  void increment() async {
+    final currentValue = state.valueOrNull;
+    if (currentValue == null) {
+      return;
+    }
+    state = const AsyncLoading<int>();
+    await Future<void>.delayed(const Duration(seconds: 1));
+    state = AsyncValue.data(currentValue + 1);
+  }
+}
+
+@riverpod
+Raw<Future<int>> fakeFirstApi(FakeFirstApiRef ref) async {
+  await Future.delayed(const Duration(seconds: 1));
+  return 1;
+}
+
+@riverpod
+Raw<Future<int>> fakeSecondApi(FakeSecondApiRef ref) async {
+  await Future.delayed(const Duration(seconds: 1));
+  return 2;
+}
+
+@riverpod
+Future<int> fakeSumApi(FakeSumApiRef ref) async {
+  final int firstApiResult = await ref.watch(fakeFirstApiProvider);
+  final int secondApiResult = await ref.watch(fakeSecondApiProvider);
+  return firstApiResult + secondApiResult;
+}
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final AsyncValue<String> greeting = ref.watch(asyncGreetingProvider);
+    return Center(
+      child: greeting.when(
+        data: (greet) => Text(greet),
+        error: (error, stack) => Text('Error: $error'),
+        loading: () => const Text("Loading..."),
+      ),
+    );
+  }
+}
+
+class Point {
+  Point(this.x, this.y);
+
+  int x;
+  int y;
 }
 
 void main() {
@@ -39,6 +98,8 @@ class MyHomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final counter = ref.watch(counterNotifierProvider);
+    final AsyncValue<String> greet = ref.watch(asyncGreetingProvider);
+    final AsyncValue<int> sumApiResult = ref.watch(fakeSumApiProvider);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -48,13 +109,28 @@ class MyHomePage extends ConsumerWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text('$counter', style: Theme.of(context).textTheme.headlineMedium),
+            greet.when(
+              loading: () => const Text('Loading'),
+              data: (greet) => Text(greet),
+              error: (e, st) => Text(e.toString()),
+            ),
+            sumApiResult.when(
+              loading: () => const Text('Loading'),
+              data: (sum) => Text('$sum'),
+              error: (e, st) => Text(e.toString()),
+            ),
+            counter.when(
+              loading: () => const Text('Loading'),
+              data: (count) => Text('$count'),
+              error: (e, st) => Text(e.toString()),
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: ref.read(counterNotifierProvider.notifier).increment,
+        onPressed: () {
+          ref.read(counterNotifierProvider.notifier).increment();
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
