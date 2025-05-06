423bdf7a8512f25c1dead2b7dbbbc08ec8ba398d
sanao1006 <naosak1006@gmail.com>
2025-05-06 21:41:42 +0900

    refactor: update counter state management to use Riverpod with CounterNotifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'main.g.dart';
+
+@riverpod
+class CounterNotifier extends _$CounterNotifier {
+  @override
+  int build() => 0;
+
+  void increment() => state++;
+}
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -27,13 +38,10 @@ class MyHomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final counter = ref.watch(counterNotificationProvider);
+    final counter = ref.watch(counterNotifierProvider);
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme
-            .of(context)
-            .colorScheme
-            .inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(title),
       ),
       body: Center(
@@ -41,33 +49,15 @@ class MyHomePage extends ConsumerWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
-            Text(
-              '$counter',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .headlineMedium,
-            ),
+            Text('$counter', style: Theme.of(context).textTheme.headlineMedium),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: ref.read(counterNotificationProvider.notifier).increment,
+        onPressed: ref.read(counterNotifierProvider.notifier).increment,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
     );
-
   }
 }
-
-class CounterNotification extends Notifier<int> {
-  @override
-  int build() => 0;
-
-  void increment() {
-    state++;
-  }
-}
-
-final counterNotificationProvider = NotifierProvider<CounterNotification, int>(() => CounterNotification());
